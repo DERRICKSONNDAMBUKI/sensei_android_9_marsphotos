0f0f8df86f9b2535a549cfa31507c53eb392529f
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-05-10 12:53:18 +0300

    Load and display

--- app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
@@ -4,11 +4,15 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import coil.load
+import com.example.android.marsphotos.R
 
-@BindingAdapter
+@BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl:String?){
     imgUrl?.let {
         val imgUrl = imgUrl.toUri().buildUpon().scheme("https").build()
-        imgView.load(imgUrl)
+        imgView.load(imgUrl){
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
     }
 }
